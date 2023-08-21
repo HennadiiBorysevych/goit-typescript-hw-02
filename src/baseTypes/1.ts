c111643b7031642ce6bec175426edbc77e93ff06
Hennadii Borysevych <genuch3@gmail.com>
2023-08-21 14:27:16 +0200

    feat:homework 2 all tasks

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,21 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+let age: number;
+let name: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+
+age = 50;
+name = "Max";
+toggle = true;
+empty = null;
+notInitialize = undefined;
+
+let callback: (a: number) => number;
+
+callback = (a) => {
+  return 100 + a;
+};
 
 export {};
--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,15 +1,17 @@
 /*
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
+interface IComponent {}
 
-class Component {
-  constructor (public props:T) {
-
-  }
+class Component<T extends IComponent> {
+  constructor(public props: T) {}
+}
+interface IPage extends IComponent {
+  title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<IPage> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
